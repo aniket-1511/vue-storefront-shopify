1e4a7bff8e58aeea9dafbc9a4528e63c18a7e8ab
Aniket Pangshe <aniket.p@borntechies.com>
2022-08-24 14:52:14 +0530

    Vue StoreFront Shopify

---
+++ packages/composables/src/useForgotPassword/index.ts
@@ -0,0 +1,21 @@
+
+import {
+  useForgotPasswordFactory,
+  UseForgotPasswordFactoryParams
+} from '@vue-storefront/core';
+import { Context } from '../types'
+
+const factoryParams: UseForgotPasswordFactoryParams<any> = {
+  resetPassword: async (context: Context, { email }) => {
+    const response = await context.$shopify.api.customerRecover({
+      email
+    })
+    return response
+  },
+  setNewPassword: async (context: Context, { tokenValue, newPassword, customQuery }) => {
+    console.log('Mocked: setNewPassword');
+    return {};
+  }
+};
+
+export default useForgotPasswordFactory<any>(factoryParams);
---
+++ packages/theme/pages/AboutUs.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="about-us">
+	  <h1 class="about-us-title">About Us</h1>
+	    <MyCustomComponent />
+    </div>
+</template>
+
+<script type="module">
+import MyCustomComponent from '~/components/MyCustomComponent.vue';
+
+export default {
+  name: 'AboutUs',
+  components: {
+    MyCustomComponent 
+  }
+};
+</script>
+
+<style lang="postcss" scoped>
+.about-us-title {
+  text-align: center;
+  color: #5ece7b;
+}
+</style>
